6f4aac67d8531b7fe1cc38ca78db17ade74df10a
SLarkZ01 <tmontoyamagon11@gmail.com>
2025-03-31 12:18:43 -0500

    se paso el login

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\loginController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get("/user", [HomeController::class, 'index'])->name('user.index');
+
+Route::get('/login', [loginController::class, 'Login'])->name('user.login');
 
 
 Route::get('/', function () {
